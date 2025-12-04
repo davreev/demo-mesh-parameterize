eaf01fa6f13537be30e35afbdb9c51062ddb75ba
David Reeves <reevdav@gmail.com>
2025-12-04 00:49:01 +0000

    Pin to Emscripten 4.0.14

--- emsc-init.ps1
+++ emsc-init.ps1
@@ -1,8 +1,9 @@
 # EMSDK_DIR is expected to be the absolute path of the Emscripten SDK root dir
 
 # Install and activate sdks/tools
-& $EMSDK_DIR/emsdk.ps1 install latest
-& $EMSDK_DIR/emsdk.ps1 activate latest
+$EMSDK_VER="4.0.14"
+& $EMSDK_DIR/emsdk.ps1 install $EMSDK_VER
+& $EMSDK_DIR/emsdk.ps1 activate $EMSDK_VER
 
 # Setup environment variables
 . $EMSDK_DIR/emsdk_env.ps1
